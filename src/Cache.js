11fb3082972ea35b230777875a1f522b5aa57644
吴劲韬 <601595686@qq.com>
2016-08-25 23:02:00 +0800

    Revert "发现cache有点bug，没修复完"
    This reverts commit 3f992e7135d8ea617414b94d5cd786f1d0614df0.

--- src/Cache.js
+++ src/Cache.js
@@ -7,10 +7,9 @@ const DispatchCenter = require('./DispatchCenter');
 /*数据缓存模块*/
 
 /**
- * 保存缓存到的数据,和两个回调方法
+ * 数据缓存类
  */
 const cacheData = {};
-
 
 /**
  * 缓存指定路径上的数据
@@ -20,46 +19,34 @@ const cacheData = {};
  * @return {undefined}
  */
 function cache(address, onReceive, onRequest) {
-    if (!address in cacheData) {  //确保不会重复注册
-        cacheData[address] = {data: undefined, onReceive, onRequest};
-    }
-}
+    DispatchCenter.receive('__cache__receive.' + address,data => {     //注册数据接收器
+        if(onReceive)
+            data = onReceive(data,cacheData[address]);
+        cacheData[address] = data;
+    });
 
+    DispatchCenter.receive('__cache__request.'+ address, callback_address => {     //注册请求接受器
+
+        let data = cacheData[address];
+
+        if(onRequest)
+            data = onRequest(data);
+
+        DispatchCenter.send(callback_address,data,false);
+    });
+}
 
 /**
  * 请求cache中的数据
  * @param {string} address 数据传输的路径
  * @param {string} callback_address 回传数据的传输路径
  */
-function requestCache(address, callback_address = '') {
-    let result;
-
-    if (address in cacheData) {
-        result = cacheData[address].data;
-        if (cacheData[address].onRequest)
-            result = cacheData[address].onRequest(result);
-    }
-
-    DispatchCenter.send(callback_address, result);
-}
-
-/**
- * 设置缓存中的数据，这个只对内部使用
- * @param {string} address 数据传输的路径
- * @param data 要保存的数据
- */
-function receiveCache(address, data) {
-    if (address in cacheData) {
-        if (cacheData[address].onReceive)
-            data = cacheData[address].onReceive(data, cacheData[address].data);
-
-        cacheData[address].data = data;
-    }
+function requestCache(address,callback_address) {
+    DispatchCenter.send('__cache__request.' + address,callback_address,false);
 }
 
 module.exports = {
     cacheData,
     cache,
-    requestCache,
-    receiveCache
+    requestCache
 };
