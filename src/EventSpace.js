2915caac3255510cf4d9f3499dfbd804b89bcdcc
wujingtao <601595686@qq.com>
2016-12-19 15:51:45 +0800

    修改了一下文档说明，还检查出了几个小错误

--- src/EventSpace.js
+++ src/EventSpace.js
@@ -2,7 +2,7 @@
  * Created by wujingtao on 2016/12/17 0017.
  */
 
-const eventLevel = new (require('./EventLevel'));
+var eventLevel = new (require('./EventLevel'));
 
 
 /**
@@ -21,34 +21,34 @@ function convertPathType(path) {
 /**
  * 注册事件监听器
  * 别名 on
- * @param {string|Array} path 接收哪一条路径上的事件.可以为字符串或数组(字符串通过‘.’来分割层级)
- * @param {function} receiver 接收到事件后执行的回调函数 ,回调函数接受两个参数（data:数据，path:路径字符串数组）
+ * @param {string|Array} eventName 接收事件的名称.可以为字符串或数组(字符串通过‘.’来分割层级)
+ * @param {function} receiver 接收到事件后执行的回调函数 ,回调函数接受两个参数（data:数据，eventName:事件的名称数组）
  * @return {function} 返回 receiver
  */
-function receive(path, receiver) {
-    path = convertPathType(path);
+function receive(eventName, receiver) {
+    eventName = convertPathType(eventName);
 
-    eventLevel.addReceiver(path, receiver);
+    eventLevel.addReceiver(eventName, receiver);
     return receiver;
 }
 
 /**
  * 注册只接收一次的事件监听器
  * 别名 once
- * @param {string|Array} path 接收哪一条路径上的事件.可以为字符串或数组(字符串通过‘.’来分割层级)
- * @param {function} receiver 接收到数据后执行的回调函数 ,回调函数接受两个参数（data:数据，path:路径字符串数组）
+ * @param {string|Array} eventName 接收事件的名称.可以为字符串或数组(字符串通过‘.’来分割层级)
+ * @param {function} receiver 接收到数据后执行的回调函数 ,回调函数接受两个参数（data:数据，eventName:事件的名称数组）
  * @return {function} 返回 receiver
  */
-function receiveOnce(path, receiver) {
+function receiveOnce(eventName, receiver) {
     if (typeof receiver !== 'function')  /*验证数据类型*/
         throw new Error('receiver is not a function');
 
-    path = convertPathType(path);
-    path.push(`${Math.random()}`.split('0')[1]);  //确保只删除自身
+    eventName = convertPathType(eventName);
+    eventName.push(Math.random().toString().split('.')[1]);  //确保只删除自身
 
-    receive(path, function (d, p) {
+    receive(eventName, function (d, p) {
         receiver(d, p);
-        cancel(path);
+        cancel(eventName);
     });
     return receiver;
 }
@@ -56,24 +56,25 @@ function receiveOnce(path, receiver) {
 /**
  * 注销数据接收器
  * 别名 off
- * @param {string|Array} path 注销哪一条路径，以及它的子级.可以为字符串或数组(字符串通过‘.’来分割层级)
+ * @param {string|Array} eventName 注销事件接收器的名称.可以为字符串或数组(字符串通过‘.’来分割层级)
  * @return {undefined}
  */
-function cancel(path) {
-
-    path = convertPathType(path);
-    eventLevel.removeReceiver(path);
+function cancel(eventName) {
+    eventName = convertPathType(eventName);
+    eventLevel.removeReceiver(eventName);
 }
 
 /**
- * 向指定路径发送消息
- * @param {string|Array} path 向哪一条路径发送数据.可以为字符串或数组(字符串通过‘.’来分割层级)
+ * 触发指定的事件接收器
+ * 别名 trigger
+ * @param {string|Array} eventName 要触发的事件名称.可以为字符串或数组(字符串通过‘.’来分割层级)
  * @param data 要发送的数据
  * @param _this 要为监听器绑定的this对象
  * @return {undefined}
  */
-function send(path, data, _this) {
-    eventLevel.trigger(path, data, _this);
+function send(eventName, data, _this) {
+    eventName = convertPathType(eventName);
+    eventLevel.trigger(eventName, data, _this);
 }
 
 module.exports = {
@@ -82,7 +83,9 @@ module.exports = {
 
     receiveOnce,
     once: receiveOnce,
+
     send,
+    trigger: send,
 
     cancel,
     off: cancel
