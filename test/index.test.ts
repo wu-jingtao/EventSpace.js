aa2aac6111d023c0c3643af2e09147cb60e376a4
wujingtao <601595686@qq.com>
2017-11-25 21:03:52 +0800

    同步

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,13 +3,28 @@ import _ = require('lodash');
 import es = require('..');
 const { receive, send, cancel, receiveOnce, EventSpace } = es;
 
+/** 最近一次测试结果
+  test global event space
+    √ test event level
+    √ test event level. Using array
+    √ test cancel
+    √ test cancel.Using array
+    √ test multi register
+    √ test multi register.Using array
+    √ test receiveOnce
+    √ test cancel and receiveOnce
+
+
+  8 passing (27ms)
+ */
+
 describe('test global event space', function () {
 
     beforeEach('clear all event listener', function () {
         cancel();
     });
 
-    it('test event level', function () {
+    it('test event level', function (done) {
         let cycle = 0;
 
         receive('test', (data) => {
@@ -37,6 +52,7 @@ describe('test global event space', function () {
                 expect(data).to.be.equal('b');
             } else {
                 expect(data).to.be.equal('c');
+                done();
             }
         });
 
@@ -49,7 +65,7 @@ describe('test global event space', function () {
         send('test.2.3', 'c');
     });
 
-    it('test event level. Using array', function () {
+    it('test event level. Using array', function (done) {
         let cycle = 0;
 
         receive(['test'], (data) => {
@@ -77,6 +93,7 @@ describe('test global event space', function () {
                 expect(data).to.be.equal('b');
             } else {
                 expect(data).to.be.equal('c');
+                done();
             }
         });
 
@@ -131,7 +148,7 @@ describe('test global event space', function () {
         send(['test', '2', '3'], 'c');
     });
 
-    it('test multi register', function () {
+    it('test multi register', function (done) {
         let cycle = 0;
 
         receive('test', (data) => {
@@ -155,6 +172,7 @@ describe('test global event space', function () {
                 expect(data).to.be.equal('a');
             } else if (cycle === 1) {
                 expect(data).to.be.equal('b');
+                done();
             } else {
                 expect().fail(`"test.2" can\`t be triggered in cycle ${cycle}`);
             }
@@ -169,7 +187,7 @@ describe('test global event space', function () {
         send('test.2.3', 'c');
     });
 
-    it('test multi register.Using array', function () {
+    it('test multi register.Using array', function (done) {
         let cycle = 0;
 
         receive(['test'], (data) => {
@@ -193,6 +211,7 @@ describe('test global event space', function () {
                 expect(data).to.be.equal('a');
             } else if (cycle === 1) {
                 expect(data).to.be.equal('b');
+                done();
             } else {
                 expect().fail(`"test.2" can\`t be triggered in cycle ${cycle}`);
             }
@@ -231,6 +250,7 @@ describe('test global event space', function () {
         });
 
         receiveOnce('test.2', (data) => {
+            expect().fail(`"test.2" can\`t be triggered`);
         });
 
         receiveOnce('test', (data) => {
