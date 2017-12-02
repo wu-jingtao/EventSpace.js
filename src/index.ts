678d505a4b769276c0103e965d8e25746937df3e
wujingtao <601595686@qq.com>
2017-12-02 22:31:35 +0800

    修复了一个类型声明错误

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { EventSpace } from "./classes/EventSpace";
 import { EventSpaceType } from "./interfaces/EventSpaceType";
 
-const es: EventSpaceType & { EventSpace: EventSpace } = {
+const es: EventSpaceType & { EventSpace: typeof EventSpace } = {
     EventSpace,
     ... new EventSpace()
 } as any;
