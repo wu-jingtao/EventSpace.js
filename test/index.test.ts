f4613375d26cb847616773f3bd118b411a540912
wujingtao <601595686@qq.com>
2017-12-04 01:21:04 +0800

    完成了测试

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,23 @@
 import expect = require('expect.js');
-import { receive, receiveOnce, cancel, cancelReverse, trigger, triggerReverse, has, hasReverse } from '../src';
+import {
+    receive,
+    receiveOnce,
+    cancel,
+    cancelDescendants,
+    cancelAncestors,
+    trigger,
+    triggerDescendants,
+    triggerAncestors,
+    has,
+    hasDescendants,
+    hasAncestors
+} from '../src';
 
 beforeEach('清除所有注册过的监听器', function () {
-    cancel();
+    cancelDescendants();
 });
 
-it('测试 receive', function () {
-
+it('测试 receive 和 trigger', function () {
     const result: number[] = [];
 
     receive([], (data) => result.push(data));
@@ -32,13 +43,12 @@ it('测试 receive', function () {
     trigger(['a', 'b', 'c', 'd'], 5);
 
     expect(result).to.eql([
-        1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4,
-        1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4
+        1, 1, 2, 2, 3, 3, 4, 4,
+        1, 1, 2, 2, 3, 3, 4, 4
     ]);
 });
 
-it('测试 receiveOnce', function () {
-
+it('测试 receiveOnce 和 trigger', function () {
     const result: number[] = [];
 
     receiveOnce([], (data) => result.push(data));
@@ -63,10 +73,10 @@ it('测试 receiveOnce', function () {
     trigger(['a', 'b', 'c'], 4);
     trigger(['a', 'b', 'c', 'd'], 5);
 
-    expect(result).to.eql([1, 1, 1, 1, 1, 1, 1, 1]);
+    expect(result).to.eql([1, 1, 2, 2, 3, 3, 4, 4]);
 });
 
-it('测试 trigger 不触发子级', function () {
+it('测试 triggerDescendants', function () {
     const result: number[] = [];
 
     receive([], (data) => result.push(data));
@@ -74,25 +84,25 @@ it('测试 trigger 不触发子级', function () {
     receive('a.b', (data) => result.push(data));
     receive('a.b.c', (data) => result.push(data));
 
-    trigger([], 1, false);
-    trigger('a', 2, false);
-    trigger('a.b', 3, false);
-    trigger('a.b.c', 4, false);
-    trigger('a.b.c.d', 5, false);
+    triggerDescendants([], 1);
+    triggerDescendants('a', 2);
+    triggerDescendants('a.b', 3);
+    triggerDescendants('a.b.c', 4);
+    triggerDescendants('a.b.c.d', 5);
 
-    trigger([], 1, false);
-    trigger(['a'], 2, false);
-    trigger(['a', 'b'], 3, false);
-    trigger(['a', 'b', 'c'], 4, false);
-    trigger(['a', 'b', 'c', 'd'], 5, false);
+    triggerDescendants([], 1, false);
+    triggerDescendants(['a'], 2, false);
+    triggerDescendants(['a', 'b'], 3, false);
+    triggerDescendants(['a', 'b', 'c'], 4, false);
+    triggerDescendants(['a', 'b', 'c', 'd'], 5, false);
 
     expect(result).to.eql([
-        1, 2, 3, 4,
-        1, 2, 3, 4
+        1, 1, 1, 1, 2, 2, 2, 3, 3, 4,
+        1, 1, 1, 2, 2, 3
     ]);
 });
 
-it('测试 triggerReverse', function () {
+it('测试 triggerAncestors', function () {
     const result: number[] = [];
 
     receive([], (data) => result.push(data));
@@ -100,76 +110,37 @@ it('测试 triggerReverse', function () {
     receive('a.b', (data) => result.push(data));
     receive('a.b.c', (data) => result.push(data));
 
-    triggerReverse([], 1);
-    triggerReverse('a', 2);
-    triggerReverse('a.b', 3);
-    triggerReverse('a.b.c', 4);
-    triggerReverse('a.b.c.d', 5);
+    triggerAncestors([], 1);
+    triggerAncestors('a', 2);
+    triggerAncestors('a.b', 3);
+    triggerAncestors('a.b.c', 4);
+    triggerAncestors('a.b.c.d', 5);
 
-    triggerReverse([], 1);
-    triggerReverse(['a'], 2);
-    triggerReverse(['a', 'b'], 3);
-    triggerReverse(['a', 'b', 'c'], 4);
-    triggerReverse(['a', 'b', 'c', 'd'], 5);
+    triggerAncestors([], 1, false);
+    triggerAncestors(['a'], 2, false);
+    triggerAncestors(['a', 'b'], 3, false);
+    triggerAncestors(['a', 'b', 'c'], 4, false);
+    triggerAncestors(['a', 'b', 'c', 'd'], 5, false);
 
     expect(result).to.eql([
         1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
-        1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5
+        2, 3, 3, 4, 4, 4, 5, 5, 5, 5
     ]);
 });
 
-describe('测试 cancel', function () {
-    it('只清除指定级别的监听器（不清除子级）', function () {
-        const result: number[] = [];
-
-        receive([], (data) => result.push(data));
-        receive('a', (data) => result.push(data));
-        receiveOnce('a.b', (data) => result.push(data));
-        receive('a.b.c', (data) => result.push(data));
-
-        cancel('a', false);
-
-        trigger([], 1);
-        trigger(['a'], 2);
-        trigger(['a', 'b'], 3);
-        trigger(['a', 'b', 'c'], 4);
-        trigger(['a', 'b', 'c', 'd'], 5);
-
-        expect(result).to.eql([1, 1, 1, 2, 3, 4]);
-    });
-
-    it('清除指定级别的特定事件监听器', function () {
-        const result: number[] = [];
-
-        function func(data: any) { result.push(data) }
-        receive('a', func);
-
-        receive([], (data) => result.push(data));
-        receive('a', (data) => result.push(data));
-        receiveOnce('a.b', (data) => result.push(data));
-        receive('a.b.c', (data) => result.push(data));
-
-        cancel('a', func);
-
-        trigger([], 1);
-        trigger(['a'], 2);
-        trigger(['a', 'b'], 3);
-        trigger(['a', 'b', 'c'], 4);
-        trigger(['a', 'b', 'c', 'd'], 5);
-
-        expect(result).to.eql([1, 1, 1, 1, 2, 2, 3, 4]);
-    });
-});
-
-it('测试 cancelReverse', function () {
+it('测试 cancel', function () {
     const result: number[] = [];
+    const func = (data: any) => result.push(data);
 
     receive([], (data) => result.push(data));
     receive('a', (data) => result.push(data));
+    receive('a', func);
+    receive('a.b', (data) => result.push(data));
     receiveOnce('a.b', (data) => result.push(data));
     receive('a.b.c', (data) => result.push(data));
 
-    cancelReverse('a.b');
+    cancel('a', func);
+    cancel('a.b');
 
     trigger([], 1);
     trigger(['a'], 2);
@@ -177,49 +148,116 @@ it('测试 cancelReverse', function () {
     trigger(['a', 'b', 'c'], 4);
     trigger(['a', 'b', 'c', 'd'], 5);
 
-    expect(result).to.eql([1, 2, 3, 4]);
+    expect(result).to.eql([1, 2, 4]);
 });
 
-describe('测试 has', function () {
-    it('判断指定级别，以及其子级，是否注册的有事件监听器', function () {
-        receive('a.b.c', () => { });
+it('测试 cancelDescendants', function () {
+    const result: number[] = [];
 
-        expect(has('a')).to.be.ok();
-        expect(has('a.b')).to.be.ok();
-        expect(has('a.b.c')).to.be.ok();
-        expect(has('a.b.c.d')).to.not.be.ok();
-    });
+    receive([], (data) => result.push(data));
+    receive('a', (data) => result.push(data));
+    receiveOnce('a.b', (data) => result.push(data));
+    receive('a.b.c', (data) => result.push(data));
 
-    it('判断指定级别，不包括其子级，是否注册的有事件监听器', function () {
-        receive('a.b.c', () => { });
+    cancelDescendants('a.b');
 
-        expect(has('a', false)).to.not.be.ok();
-        expect(has('a.b', false)).to.not.be.ok();
-        expect(has('a.b.c', false)).to.be.ok();
-        expect(has('a.b.c.d', false)).to.not.be.ok();
-    });
+    trigger([], 1);
+    trigger(['a'], 2);
+    trigger(['a', 'b'], 3);
+    trigger(['a', 'b', 'c'], 4);
+    trigger(['a', 'b', 'c', 'd'], 5);
 
-    it('判断指定级别是否注册的有特定事件监听器', function () {
-        function test() { }
-        receive('a', test);
+    cancelDescendants([], false);
 
-        receive('a', () => { });
-        receive('a.b', () => { });
-        receive('a.b.c', () => { });
+    trigger([], 1);
+    trigger(['a'], 2);
+    trigger(['a', 'b'], 3);
+    trigger(['a', 'b', 'c'], 4);
+    trigger(['a', 'b', 'c', 'd'], 5);
 
-        expect(has('a', test)).to.be.ok();
-        expect(has('a.b', test)).to.not.be.ok();
-        expect(has('a.b.c', test)).to.not.be.ok();
-        expect(has('a.b.c.d', test)).to.not.be.ok();
-    });
+    expect(result).to.eql([
+        1, 2,
+        1
+    ]);
 });
 
-it('测试 hasReverse', function () {
+it('测试 cancelAncestors', function () {
+    const result: number[] = [];
+
+    receive([], (data) => result.push(data));
+    receive('a', (data) => result.push(data));
+    receiveOnce('a.b', (data) => result.push(data));
+    receive('a.b.c', (data) => result.push(data));
+
+    cancelAncestors([]);
+
+    trigger([], 1);
+    trigger(['a'], 2);
+    trigger(['a', 'b'], 3);
+    trigger(['a', 'b', 'c'], 4);
+    trigger(['a', 'b', 'c', 'd'], 5);
+
+    cancelAncestors('a.b.c', false);
+
+    trigger([], 1);
+    trigger(['a'], 2);
+    trigger(['a', 'b'], 3);
+    trigger(['a', 'b', 'c'], 4);
+    trigger(['a', 'b', 'c', 'd'], 5);
+
+    expect(result).to.eql([
+        2, 3, 4,
+        4
+    ]);
+});
+
+it('测试 has', function () {
+    const func = () => { };
+    receive('a.b', func);
     receive('a.b.c', () => { });
-    
-    expect(hasReverse([])).to.not.be.ok();
-    expect(hasReverse('a')).to.not.be.ok();
-    expect(hasReverse('a.b')).to.not.be.ok();
-    expect(hasReverse('a.b.c')).to.be.ok();
-    expect(hasReverse('a.b.c.d')).to.be.ok();
+
+    expect(has('a')).to.not.be.ok();
+    expect(has('a.b')).to.be.ok();
+    expect(has('a.b', func)).to.be.ok();
+    expect(has('a.b.c')).to.be.ok();
+    expect(has('a.b.c', func)).to.not.be.ok();
+    expect(has('a.b.c.d')).to.not.be.ok();
+});
+
+it('测试 hasDescendants', function () {
+    receive([], () => { });
+    receive('a', () => { });
+    receive('a.b', () => { });
+    receive('a.b.c', () => { });
+
+    expect(hasDescendants([])).to.be.ok();
+    expect(hasDescendants('a')).to.be.ok();
+    expect(hasDescendants('a.b')).to.be.ok();
+    expect(hasDescendants('a.b.c')).to.be.ok();
+    expect(hasDescendants('a.b.c.d')).to.not.be.ok();
+
+    expect(hasDescendants([], false)).to.be.ok();
+    expect(hasDescendants('a', false)).to.be.ok();
+    expect(hasDescendants('a.b', false)).to.be.ok();
+    expect(hasDescendants('a.b.c', false)).to.not.be.ok();
+    expect(hasDescendants('a.b.c.d', false)).to.not.be.ok();
+});
+
+it('测试 hasAncestors', function () {
+    receive([], () => { });
+    receive('a', () => { });
+    receive('a.b', () => { });
+    receive('a.b.c', () => { });
+
+    expect(hasAncestors([])).to.be.ok();
+    expect(hasAncestors('a')).to.be.ok();
+    expect(hasAncestors('a.b')).to.be.ok();
+    expect(hasAncestors('a.b.c')).to.be.ok();
+    expect(hasAncestors('a.b.c.d')).to.be.ok();
+
+    expect(hasAncestors([], false)).to.not.be.ok();
+    expect(hasAncestors('a', false)).to.be.ok();
+    expect(hasAncestors('a.b', false)).to.be.ok();
+    expect(hasAncestors('a.b.c', false)).to.be.ok();
+    expect(hasAncestors('a.b.c.d', false)).to.be.ok();
 });
